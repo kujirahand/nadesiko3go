aee812277ab4ab77d7474d2b27da93deca684925
kujirahand <kujira@kujirahand.com>
2020-05-24 13:33:33 +0900

    パスを修正

--- make.sh
+++ make.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 BASE_DIR=$(cd $(dirname $0); pwd)
-FILE_OUT=$BASE_DIR/bin/cnako3go
-SRC_DIR=$BASE_DIR/src/nako3
+FILE_OUT=$BASE_DIR/cnako3go
+PARSER_DIR=$BASE_DIR/parser
 
-cd $SRC_DIR
-go build -o $FILE_OUT
+echo "--- build parser ---"
+cd $PARSER_DIR
+cnako3 extract_token.nako3
+goyacc _parser_generated.y
 cd $BASE_DIR
 
-
-
+echo "--- build cnako3go ---"
+go build -o $FILE_OUT
 
--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 BASE_DIR=$(cd $(dirname $0); pwd)
-CNAKO=$BASE_DIR/bin/cnako3go
+CNAKO3GO=$BASE_DIR/cnako3go
 FMAKE=$BASE_DIR/make.sh
-SRC_DIR=$BASE_DIR/src/nako3
 TEST_DIR=$BASE_DIR/test
 
 # build
@@ -10,9 +9,8 @@ $FMAKE
 
 # test
 echo "--- fizzbizz ---"
-$CNAKO -d $TEST_DIR/fizzbuzz.nako3
+$CNAKO3GO -d $TEST_DIR/fizzbuzz.nako3
 echo "--- basic ---"
-$CNAKO $TEST_DIR/basic.nako3
-
+$CNAKO3GO $TEST_DIR/basic.nako3
 
 
