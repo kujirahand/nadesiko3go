d41102a43970bb2c492fff53566df1aae25c5d2d
kujirahand <kujira@kujirahand.com>
2020-06-25 21:57:30 +0900

    CSV関連のモジュールの追加忘れ

---
+++ func/system/csvvalue.go
@@ -0,0 +1,76 @@
+package system
+
+import (
+	"strings"
+
+	"github.com/kujirahand/nadesiko3go/value"
+)
+
+// GetCSVToValue : CSV文字列をValueに変換
+func GetCSVToValue(csv string, splitter rune) value.Value {
+	res := value.NewValueArray()
+	// 改行コードを統一
+	csv = strings.ReplaceAll(csv, "\r\n", "\n")
+	csv = strings.ReplaceAll(csv, "\r", "\n")
+	// 解析開始
+	cols := value.NewValueArrayPtr()
+	col := ""
+	insideQuote := false
+	// 最終的に改行を追加する
+	if csv != "" && csv[len(csv)-1] != '\n' { // 末尾に改行がなければ改行を追加
+		csv += "\n"
+	}
+	src := []rune(csv)
+	i := 0
+	for i < len(src) {
+		c := src[i]
+		if !insideQuote {
+			if c == splitter {
+				pcol := value.NewValueStrPtr(col)
+				cols.Append(pcol)
+				col = ""
+				i++
+				continue
+			}
+			if col == "" && c == '"' {
+				insideQuote = true
+				i++
+				continue
+			}
+			if c == '\n' {
+				pcol := value.NewValueStrPtr(col)
+				cols.Append(pcol)
+				res.Append(cols)
+				println(res.ToJSONString())
+				col = ""
+				cols = value.NewValueArrayPtr()
+				i++
+				continue
+			}
+			col += string(c)
+			i++
+			continue
+		}
+		// inside quote
+		if c == '"' {
+			if i+1 < len(src) && src[i+1] == '"' {
+				col += "\""
+				i += 2
+				continue
+			}
+			i++ // skip "
+			insideQuote = false
+			for i < len(src) { // skip white space
+				if src[i] == ' ' {
+					i++
+					continue
+				}
+				break
+			}
+			continue
+		}
+		col += string(c)
+		i++
+	}
+	return res
+}
